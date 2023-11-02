dde9f7fab5cc8b3a0f8c1a0e1508c0d5e8b4aade
Octavian Andrei Măcelaru <octavian-andrei.macelaru@s.unibuc.ro>
2023-11-02 23:02:55 +0200

    add DBController

---
+++ Controllers/DataBaseController.cs
@@ -0,0 +1,18 @@
+﻿using DAW_DOTNET_Laborator.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAW_DOTNET_Laborator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataBaseController : ControllerBase
+    {
+        private readonly StudentContext _studentContext;
+
+        public DataBaseController(StudentContext studentContext)
+        {
+            _studentContext = studentContext;
+        }
+
+    }
+}
